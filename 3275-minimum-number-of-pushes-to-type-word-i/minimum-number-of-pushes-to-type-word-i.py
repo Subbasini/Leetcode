b086da5b7a38026cddb0fc6fa9f1440619c04147
Subbasini <subbasini1706@gmail.com>
2026-08-03 23:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.64%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n=len(word)
+        res=0
+        
+        for i in range(n):
+            res+=(i//8)+1
+
+        return res
